45f1d41271de7e86857ccf0ab6c2ad016415c7d6
上官镜文 <shangguanjingwen@zj.tech>
2023-06-21 20:00:21 +0800

    引入nacos配置

--- src/main/java/com/bruce/course/shardingjdbccourse/ShardingJdbcCourseApplication.java
+++ src/main/java/com/bruce/course/shardingjdbccourse/ShardingJdbcCourseApplication.java
@@ -2,8 +2,10 @@ package com.bruce.course.shardingjdbccourse;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.bruce"})
 public class ShardingJdbcCourseApplication {
 
     public static void main(String[] args) {
